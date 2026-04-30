da7b32275154cd0ff51e7bc2c667e8e5db574244
Umar Farooq <umarfarooq1234@gmail.com>
2026-04-30 11:38:47 +0930

    fixing

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 provider "azurerm" {
-    features {}
-    
-    subscription_id = "55e255f2-e37a-47b4-bdc9-8d1f4a8d391c"
+  features {}
+
+  subscription_id = "55e255f2-e37a-47b4-bdc9-8d1f4a8d391c"
 
 }
