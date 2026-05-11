33ccfd9014318ceea0c7b3e62c9377481d29ddec
Umar Farooq <umarfarooq1234@gmail.com>
2026-05-11 23:20:37 +0930

    manual approval

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -5,6 +5,15 @@ terraform {
       version = "4.70.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rgStorage"
+    storage_account_name = "umarfarooq0000"
+    container_name       = "umarfarooqcontainer"
+    key                  = "dev.terraform.tfstate"
+
+  }
+
 }
 
 provider "azurerm" {
